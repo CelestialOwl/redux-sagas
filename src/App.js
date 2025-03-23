82d7b42f903d857d98500af260fbccbcbca86d75
hassan <cm.hassan98@gmail.com>
2025-03-24 01:55:24 +0500

    redux saga initial setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 
 import Header from './components/Header';
 import ImageGrid from './components/ImageGrid';
 
+import configStore from './store';
+const store = configStore()
+
 class App extends Component {
     render() {
         return (
-            <div>
-                <Header />
-                <ImageGrid />
-            </div>
+            <Provider store={store}>
+                <div>
+                    <Header />
+                    <ImageGrid />
+                </div>
+            </Provider >
         );
     }
 }
